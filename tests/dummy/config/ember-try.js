0247a493a68b96669310112d963b78c2630fb8a5
Robbie Wagner <rwwagner90@gmail.com>
2024-03-25 08:37:33 -0400

    yarn -> pnpm

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -5,7 +5,7 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
-    useYarn: true,
+    usePnpm: true,
     scenarios: [
       {
         name: 'ember-lts-4.8',
@@ -20,6 +20,14 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.4.0',
           },
         },
       },
